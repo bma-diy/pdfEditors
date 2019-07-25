7e608ee7ad9ea430275c2b4c1a6c9d26f3d8a565
Billy Manashi <43049588+bma-diy@users.noreply.github.com>
2019-07-25 10:35:38 -0700

    Add files via upload

---
+++ pdfRotator.py
@@ -0,0 +1,19 @@
+
+
+import PyPDF2
+import os
+
+pdf_in = open('original.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(pdf_in)
+pdf_writer = PyPDF2.PdfFileWriter()
+ 
+for pagenum in range(pdf_reader.numPages):
+    page = pdf_reader.getPage(pagenum)
+    if pagenum % 2:
+        page.rotateClockwise(180)
+    pdf_writer.addPage(page)
+ 
+pdf_out = open('rotated.pdf', 'wb')
+pdf_writer.write(pdf_out)
+pdf_out.close()
+pdf_in.close()
